7d99c8e2036a5e3466864b99a224ab6cbf277bcb
Kevin <kevin.ayers123@icloud.com>
2021-01-13 20:14:05 -0800

    began payment processing setup

--- src/Payment.js
+++ src/Payment.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import CheckoutProduct from "./CheckoutProduct";
 import "./Payment.css";
 import { useStateValue } from "./StateProvider";
-import { Link } from "react-router-dom";
+import { getBasketTotal } from "./reducer";
+import CurrencyFormat from "react-currency-format";
+import { Link, useHistory } from "react-router-dom";
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 
 function Payment() {
   const [{ basket, user }, dispatch] = useStateValue();
+  const [error, setError] = useState();
+  const [disabled, setDisabled] = useState();
+  const stripe = useStripe();
+  const elements = useElements();
+  const history = useHistory();
+
+  const [succeeded, setSucceeded] = useState(false);
+  const [processing, setProcessing] = useState("");
+  const [clientSecret, setClientSecret] = useState(true);
+
+  const handleSubmit = (e) => {
+    // do all the fancy stripe stuff
+  };
+
+  const handleChange = (event) => {
+    //listen for changes in card and display any errors as they are typed
+    setDisabled(event.empty);
+    setError(event.error ? event.error.message : "");
+  };
 
   return (
     <div className="payment">
@@ -14,13 +36,13 @@ function Payment() {
           Checkout (<Link to="/checkout">{basket?.length} items</Link>)
         </h1>
         <div className="payment__section">
-          <div calssName="payment__title">
+          <div className="payment__title">
             <h3>Delivery Address</h3>
           </div>
           <div className="payment__address">
             <p>{user?.email}</p>
-            <p>123 React Lane</p>
-            <p>Los Angeles, CA</p>
+            <p>Test React Land</p>
+            <p>Seoul, Korea</p>
           </div>
         </div>
         <div className="payment__section">
@@ -43,7 +65,29 @@ function Payment() {
           <div className="payment__title">
             <h3>Payment Method</h3>
           </div>
-          <div className="payment__details"></div>
+          <div className="payment__details">
+            <form onSubmit={handleSubmit}>
+              <CardElement onChange={handleChange} />
+              <div className="payment__priceContainer">
+                <CurrencyFormat
+                  renderText={(value) => (
+                    <>
+                      <h3>Order Total: {value}</h3>
+                    </>
+                  )}
+                  decimalScale={2}
+                  value={getBasketTotal(basket)} // part of the homework
+                  displayType={"text"}
+                  thousandSeparator={true}
+                  prefix={"$"}
+                />
+                <button disabled={processing || disabled || succeeded}>
+                  <span>{processing ? <p>Processing</p> : "Buy Now"}</span>
+                </button>
+              </div>
+              {error && <div>{error}</div>}
+            </form>
+          </div>
         </div>
       </div>
     </div>
